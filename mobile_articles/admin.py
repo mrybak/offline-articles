5bc36f2a8b7f3daf1b77d036c9b858be1b106c8b
Michał Rybak <mr321163@students.mimuw.edu.pl>
2013-06-19 19:31:01 +0200

    auto_add_date

--- mobile_articles/admin.py
+++ mobile_articles/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
 from mobile_articles.models import Article
 
-class AuthorAdmin(admin.ModelAdmin):
-    exclude = ('pub_date',)
-
-admin.site.register(Article, AuthorAdmin)
+admin.site.register(Article)
--- mobile_articles/models.py
+++ mobile_articles/models.py
@@ -6,6 +6,6 @@ from django import forms
 class Article(models.Model):
     title = models.CharField(max_length=300)
     content = models.TextField()
-    pub_date = models.DateTimeField(default=datetime.datetime.now())
+    pub_date = models.DateTimeField(auto_now_add=True)
     def __unicode__(self):
         return self.title + " (" + str(self.pub_date) + ")"
